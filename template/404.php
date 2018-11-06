2c52bf91194d9ca42e71349103f5585670f672d4
Borja <1415.romero.borja@gmail.com>
2018-11-06 15:36:52 +0100

    update

--- template/404.php
+++ template/404.php
@@ -5,7 +5,7 @@
             <h2 class="title has-text-centered">Houston, we have a problem!</h2>
             <div class="intro column is-8 is-offset-2">
                 <br>
-                <p class="subtitle has-text-centered">
+                <p class="subtitle is-centered">
                     Se ha producido un error en la página. Por favor vuelva a intentarlo.
                 </p>
             </div>
