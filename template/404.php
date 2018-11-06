709186fc45824660d76daa30289c6553f3b0702f
Borja <1415.romero.borja@gmail.com>
2018-11-06 15:23:54 +0100

    Hola

--- template/404.php
+++ template/404.php
@@ -3,11 +3,11 @@
 </head>
 <body>
     <div class="hero-body">
-        <div class="container">
-            <h1 class="title has-text-centered">Houston, we have a problem!</h1>
+        <div class="container has-text-centered">
+            <h1 class="title">Houston, we have a problem!</h1>
             <div class="intro column is-8 is-offset-2">
                 <br>
-                <p class="subtitle has-text-centered">
+                <p class="subtitle>
                     Se ha producido un error en la página. Por favor vuelva a intentarlo.
                 </p>
             </div>
