63157b924e0a0f8d3c62e9e064cebc0da5ab0f8d
Borja <1415.romero.borja@gmail.com>
2018-11-06 15:27:21 +0100

    update

--- template/404.php
+++ template/404.php
@@ -2,8 +2,8 @@
 <body>
     <div class="hero-body">
         <div class="container has-text-centered">
-            <h1 class="title">Houston, we have a problem!</h1>
             <div class="intro column is-8 is-offset-2">
+                <h1 class="title">Houston, we have a problem!</h1>
                 <br>
                 <p class="subtitle">Se ha producido un error en la página. Por favor vuelva a intentarlo.</p>
             </div>
