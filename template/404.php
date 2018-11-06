09e6fa557b8eb5025bf7b84bc2c021f9c6d93ec7
Borja <1415.romero.borja@gmail.com>
2018-11-06 15:25:02 +0100

    update

--- template/404.php
+++ template/404.php
@@ -5,7 +5,7 @@
             <h1 class="title">Houston, we have a problem!</h1>
             <div class="intro column is-8 is-offset-2">
                 <br>
-                <p class="subtitle>
+                <p class="subtitle">
                     Se ha producido un error en la página. Por favor vuelva a intentarlo.
                 </p>
             </div>
